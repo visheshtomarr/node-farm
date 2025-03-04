1d4371d248d445cd69c5cd251c414fa7cb63ab41
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-04 19:21:19 +0530

    Replaced placeholders in html templates

--- index.js
+++ index.js
@@ -8,24 +8,54 @@ const url = require('url');
 // only be executed once.
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const templOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const templProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+const templCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
+const replacePlaceholders = (template, product) => {
+    // Globally replace all the placeholders using "//g".
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
 // Creating a server.
 // As soon as we hit the URL page, we will get the response from our server.
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, { 'content-type': 'text/html' });
+
+        const cardsHtml = dataObj.map(product => replacePlaceholders(templCard, product)).join('');
+        const output = templOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
     }
+
+    // Product page
     else if (pathName === '/product') {
         res.end('This is the PRODUCT');
     }
+
+    // API page
     else if (pathName === '/api') {
         res.writeHead(200, { 'content-type': 'application/json' });
         // Here we are passing the 'data' instead of 'dataObj' as the sent response 
         // argument should either be string, or an instance of Buffer of Uint8Array.
         res.end(data);
     }
+
+    // Not found page
     else {
         res.writeHead(404, {
             // A header is just a piece of information about the response we 
