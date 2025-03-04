d361f03abe3ba556b2a89c39c0633dc5a99a8a67
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-04 23:16:26 +0530

    Created own module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replacePlaceholders = require('./modules/replacePlaceholders');
 
 /////////////////////////////
 // SERVER
@@ -11,22 +12,6 @@ const dataObj = JSON.parse(data);
 const templOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
 const templProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const templCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
-
-const replacePlaceholders = (template, product) => {
-    // Globally replace all the placeholders using "//g".
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
 
 // Creating a server.
 // As soon as we hit the URL page, we will get the response from our server.
