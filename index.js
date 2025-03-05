e9c1a115366eb8adcf737c2ff0ae9c89c5ec06a0
visheshtomarr <visheshtomar7800@gmail.com>
2025-03-05 17:00:55 +0530

    Refactored code to slugify urls

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const replacePlaceholders = require('./modules/replacePlaceholders');
+const slugify = require('slugify');
 
 /////////////////////////////
 // SERVER
@@ -13,27 +14,43 @@ const templOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'u
 const templProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const templCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
+// Creating slugs for our products urls.
+const dataWithSlugs = dataObj.map(product => ({
+    ...product,
+    slug: slugify(product.productName, { lower: true }) 
+}));
+// console.log(dataWithSlugs);
+
 // Creating a server.
 // As soon as we hit the URL page, we will get the response from our server.
 const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
-    // console.log(query, pathname);
+    const { pathname } = url.parse(req.url, true);
+    // console.log(pathname);
     
     // Overview page
     if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'content-type': 'text/html' });
 
-        const cardsHtml = dataObj.map(product => replacePlaceholders(templCard, product)).join('');
+        const cardsHtml = dataWithSlugs.map(product => replacePlaceholders(templCard, product)).join('');
         const output = templOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
         res.end(output);
     }
 
     // Product page
-    else if (pathname === '/product') {
-        res.writeHead(200, { 'content-type': 'text/html' });
-        const product = dataObj[query.id];
-        const output = replacePlaceholders(templProduct, product);
-        res.end(output);
+    else if (pathname.startsWith('/product')) {
+        const slug = pathname.split('product/')[1];
+        // console.log(slug);
+        const product = dataWithSlugs.find(product => product.slug === slug);
+
+        if (product) {
+            res.writeHead(200, { 'content-type': 'text/html' });
+            const output = replacePlaceholders(templProduct, product);
+            res.end(output);
+        }
+        else {
+            res.writeHead(404, { 'content-type': 'text/html' });
+            res.end('<h1>Page Not Found</h1>');
+        }
     }
 
     // API page
